ddcb03e2e7772dd21c6f81d42736e42083ff2c80
Ádran Carnavale <adran.carnavale@gmail.com>
2023-05-05 16:59:08 -0300

    feat: starts api server config

---
+++ api/index.js
@@ -0,0 +1,25 @@
+import { createServer } from "node:http";
+import { createReadStream } from "node:fs";
+import { Readable } from "node:stream";
+import { WritableStream } from "node:stream/web";
+
+const PORT = 3000;
+
+createServer(async (request, response) => {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "*",
+  };
+
+  if (request.method === "OPTIONS") {
+    response.writeHead(204, headers);
+    response.end();
+    return;
+  }
+
+  createReadStream("../assets/animeflv.csv").pipe(response);
+
+  response.writeHead(200, headers);
+})
+  .listen(PORT)
+  .on("listening", (_) => console.log(`server is running at ${PORT}`));
